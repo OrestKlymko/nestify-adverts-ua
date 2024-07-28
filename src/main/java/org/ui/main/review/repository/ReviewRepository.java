2456900e172fa54aa66ddc94787066e83babd533
Orest <orest.klymko@gamo.sk>
2024-07-28 16:01:21 +0200

    Create review on adverts

--- src/main/java/org/ui/main/review/repository/ReviewRepository.java
+++ src/main/java/org/ui/main/review/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package org.ui.main.review.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,7 @@ import org.ui.main.review.model.Review;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Transactional
     @Modifying
     @Query(value = "INSERT INTO REVIEWS_ADVERTS (ADVERT_ID, REVIEW_ID) VALUES (?1, ?2)", nativeQuery = true)
     void insertReviewOnAdvert(Long advertId, Long reviewId);
