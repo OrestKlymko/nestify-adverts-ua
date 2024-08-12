d4f4ab384b0024e7160a6fa6f514a0f7d2def5b8
Orest <orest.klymko@gamo.sk>
2024-08-12 13:31:48 +0200

    Add test security

--- src/main/java/org/ui/main/MainApplication.java
+++ src/main/java/org/ui/main/MainApplication.java
@@ -2,10 +2,12 @@ package org.ui.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableDiscoveryClient
 public class MainApplication {
 
 	public static void main(String[] args) {
